57216116c7ee5e39e5d9478eebd448ba40141c28
Vasyl Pankiv <wasilius2@gmail.com>
2022-11-21 17:53:53 +0200

    initial commit

---
+++ src/main/java/com/example/ultimatesystemstesttask/UltimateSystemsTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.ultimatesystemstesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateSystemsTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltimateSystemsTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ultimatesystemstesttask/UltimateSystemsTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ultimatesystemstesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateSystemsTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
